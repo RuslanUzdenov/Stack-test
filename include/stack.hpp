bc92a32495b11634eb8e3b6cc0547487bcc1386d
RuslanUzdenov <0508rus@bk.ru>
2016-11-14 11:55:09 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -2,129 +2,41 @@
 #define stack_hpp
 #pragma once
 
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <stdexcept>
-using namespace std;
-
-size_t max(size_t a, size_t b) {
-    return a > b ? a : b;
+SCENARIO("count", "[count]"){
+  Stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
 }
 
-template <typename T>
-T* new_with_copy(const T *tmp, size_t count, size_t array_size) {  /* strong */
-    T *array_ = new T[array_size];
-    copy(tmp, tmp + count, array_);
-    return array_;
+SCENARIO("push", "[push]"){
+  Stack<int> s;
+  s.push(1);
+  REQUIRE(s.count() == 1);
+  REQUIRE(s.top() == 1);
 }
 
-template<typename T>
-class Stack {
-public:
-    Stack(); /* noexcept*/
-
-    ~Stack(); /* noexcept */
-
-    Stack(const Stack &); /* strong */
-
-    Stack& operator = (const Stack &); /* strong */
-
-    size_t count() const; /* noexcept */
-
-    void push(T const &); /* strong */
-
-    void pop();  /* strong */
-    
-    T top() const; /* strong */
-
-    bool is_empty() const; /* noexcept */
-
-private:
-    void grow(); /* strong */
-
-    T *array_;
-    size_t array_size_;
-    size_t count_;
-};
-
-template<typename T>
-Stack<T>::Stack()
-        : array_size_(0),
-          count_(0) { }
-
-template<typename T>
-Stack<T>::~Stack() {
-    if (!is_empty()) {
-        delete[] array_;
-    }
+SCENARIO("pop", "[pop]"){
+  Stack<int> s;
+  s.push(1);
+  REQUIRE(s.count() == 1);
+  REQUIRE(s.top() == 1);
 }
 
-template<typename T>
-size_t Stack<T>::count() const {
-    return count_;
+SCENARIO("prisv", "[prisv]"){
+  Stack<int> s;
+  s.push(1);
+  Stack<int> s2;
+  s2=s;
+  REQUIRE(s.count() == s2.count());
+  REQUIRE(s.top() == s2.top());
 }
 
-template<typename T>
-void Stack<T>::push(T const &element) {
-    if (array_size_ == count_) {
-        grow();
-    }
-    array_[count_++] = element;
-}
-
-template<typename T>
-void Stack<T>::grow() {
-    size_t new_array_size_ = max(1, array_size_ * 2);
-    T *new_array_ = new_with_copy(array_, count_, new_array_size_);
-    if (!is_empty()) {
-        delete[] array_;
-    }
-    array_ = new_array_;
-    array_size_ = new_array_size_;
-    return;
-}
-
-template<typename T>
-void Stack<T>::pop() {
-    if (is_empty()) {
-        throw std::logic_error("Stack is empty!");
-    }
-    --count;
-}
-
-template <typename T>
-T Stack<T>::top() const {
-    if (is_empty()) {
-        throw std::logic_error("Stack is empty!");
-    }
-    return array_[count_ - 1];
-}
-
-template<typename T>
-bool Stack<T>::is_empty() const {
-    return count_ == 0;
-}
-
-template <typename T>
-Stack<T>::Stack(const Stack &tmp)
-        :   count_(tmp.count_),
-            array_size_(tmp.array_size_)
-{
-    array_ = new_with_copy(tmp.array_, count_, array_size_);
-}
-
-template <typename T>
-Stack<T>& Stack<T>::operator=(const Stack<T> &tmp) {
-    if (this == &tmp) {
-    }   else {
-        count_ = tmp.count_;
-        array_size_ = tmp.array_size_;
-        delete[] array_;
-        array_ = new_with_copy(tmp.array_, count_, array_size_);
-    
-    }
-    return *this;
+SCENARIO("copy", "[copy]"){
+  Stack<int> s;
+  s.push(3);
+  Stack<int> s2(s);
+  REQUIRE(s2.count() == 1);
+  REQUIRE(s2.top() == 3);
 }
 
 #endif
